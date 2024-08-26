551c5437d83944012ffc069f0cb4bbd11bb22b15
Vinicius Uliano <vgarciauliano@gmail.com>
2024-08-25 21:06:21 -0300

    dev web v2

---
+++ dev_web_v2/src/components/FooterComponent.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="footer">
+    <h2>Questions? Call 000-000-000-0000</h2>
+
+    <div class="row">
+      <div class="col">
+        <a href="#">FAQ</a>
+        <a href="#">Investor Relations</a>
+        <a href="#">Privacy</a>
+        <a href="#">Speed Test</a>
+      </div>
+      <div class="col">
+        <a href="#">Help Centre</a>
+        <a href="#">Jobs</a>
+        <a href="#">Cookie Preferences</a>
+        <a href="#">Legal Notices</a>
+      </div>
+      <div class="col">
+        <a href="#">Account</a>
+        <a href="#">Ways to Watch</a>
+        <a href="#">Corporate Information</a>
+        <a href="#">Only on Netflix</a>
+      </div>
+      <div class="col">
+        <a href="#">Media Centre</a>
+        <a href="#">Terms of Use</a>
+        <a href="#">Contact Us</a>
+      </div>
+    </div>
+
+    <button class="lang-btn">
+      <i class="fa-solid fa-globe"></i> English
+      <img src="/src/assets/down-icon.png" />
+    </button>
+
+    <p class="copyright-text">Netflix United States</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+};
+</script>
+
+<style scoped>
+.footer {
+  padding: 50px 15% 10px;
+  border-top: 6px solid #333;
+  color: #777;
+}
+
+.footer h2 {
+  font-size: 18px;
+  font-weight: 400;
+  margin-bottom: 30px;
+}
+
+.footer .col {
+  flex-basis: 25%;
+  flex-grow: 1;
+  margin-bottom: 20px;
+}
+
+.footer .col a {
+  display: block;
+  color: #777;
+  font-size: 14px;
+  margin-bottom: 10px;
+  text-decoration: none;
+}
+
+.footer .row {
+  align-items: flex-start;
+  padding: 10px 0px;
+}
+
+.footer .lang-btn {
+  display: inline-flex;
+  align-items: center;
+  gap:5px;
+  background: transparent;
+  border: 1px solid #fff;
+  padding: 7px 10px;
+  color: #FFF;
+  border-radius: 3px;
+}
+
+.footer .lang-btn img {
+  width: 10px;
+  margin-left: 10px;
+}
+
+
+.copyright-text {
+  font-size: 14px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+@media only screen and (max-width: 600px) {
+  .footer .col {
+    flex-basis: 100%;
+  }
+}
+</style>
---
+++ dev_web_v2/src/components/HeaderComponent.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="header">
+    <nav>
+      <img class="logo" src="/src/assets/logo.png" alt="logo" />
+      <div>
+        <button class="lang-btn">
+          <i class="fa-solid fa-globe"></i> English
+          <img src="/src/assets/down-icon.png" />
+        </button>
+        <button>Sign In</button>
+      </div>
+    </nav>
+
+    <div class="header-content">
+      <h1>Unlimited movies, TV shows and more.</h1>
+      <h3>Watch anywhere. Cancel anytime.</h3>
+      <p>
+        Ready to watch? Enter your email to create or restart your membership.
+      </p>
+      <form action="#" class="email-signup">
+        <input type="email" placeholder="Email address" required />
+        <button type="submit">Get Started</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+};
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    url(./src/assets/header-image.png);
+  background-size: cover;
+  background-position: center;
+  padding: 10px 8%;
+  position: relative;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+}
+
+.logo {
+  width: 150px;
+  cursor: pointer;
+}
+
+nav button {
+  border: none;
+  outline: none;
+  background-color: #db0001;
+  color: #fff;
+  padding: 7px 20px;
+  font-size: 12px;
+  border-radius: 4px;
+  margin-left: 10px;
+  cursor: pointer;
+}
+
+.lang-btn {
+  display: inline-flex;
+  align-items: center;
+  background: transparent;
+  border: 1px solid #fff;
+  padding: 7px 10px;
+}
+
+.lang-btn img {
+  width: 10px;
+  margin-left: 10px;
+}
+
+.fa-globe {
+  color: #fff;
+  width: 10px;
+  margin-right: 10px;
+}
+
+.header-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  margin-top: 100px;
+}
+
+.header-content h1 {
+  font-size: 60px;
+  line-height: 70px;
+  font-weight: 600;
+  max-width: 650px;
+}
+
+.header-content h3 {
+  font-weight: 400;
+  margin-bottom: 20px;
+}
+
+.email-signup {
+  background: #fff;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+  overflow: hidden;
+}
+
+.email-signup input {
+  flex: 1;
+  border: 0;
+  outline: 0;
+  margin-left: 20px;
+}
+
+.email-signup button {
+  background-color: #db0001;
+  border: none;
+  outline: none;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+  padding: 15px 30px;
+}
+
+@media only screen and (max-width: 600px) {
+  .logo {
+    width: 100px;
+  }
+
+  nav button {
+    padding: 5px 10px;
+  }
+
+  nav .lang-btn {
+    padding: 4px 8px;
+  }
+
+  .header-content {
+    position: unset;
+    transform: none;
+    padding-top: 150px;
+  }
+
+  .header-content h1 {
+    font-size: 30px;
+  }
+
+ .email-signup button {
+    font-size: 12px;
+    padding: 10px 15px;
+  }
+}
+</style>
